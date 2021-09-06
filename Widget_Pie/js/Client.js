0b22531de3540993034fc58c139088a814f6fe39
Enrique Joya <Enrique_Joya@outlook.com>
2021-09-05 23:07:15 -0500

    ServiceNow Widget Pie with Chart.js added!

---
+++ Widget_Pie/js/Client.js
@@ -0,0 +1,10 @@
+api.controller=function() {
+	/* widget controller */
+	var c = this;
+	//alert(c.data.count);
+	//alert(c.data.count2);
+	//alert(c.data.count3);
+	c.labels = ["New", "In Progress", "On Hold"];
+	c.data = [c.data.count, c.data.count2, c.data.count3];
+	
+};
